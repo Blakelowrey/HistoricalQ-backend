9d975d6d04aa66196849c445d770efecdddbffec
Blake Lowrey <blake.lowrey1@gmail.com>
2019-08-15 22:37:22 -0400

    final

--- src/users/users-service.js
+++ src/users/users-service.js
@@ -13,7 +13,7 @@ const UsersService = {
     return db('users').where('id', id).delete();
   },
   deleteFavorite(db, user_ref, entry_ref){
-    db('favorites').where({'entry_ref': entry_ref, 'user_ref' : user_ref}).delete();
+    return db('favorites').where({'entry_ref': entry_ref, 'user_ref' : user_ref}).delete();
   },
   addFavorite(db, favorite){
     return db('favorites').insert(favorite).returning('*');
